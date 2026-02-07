af7f6c65fd8f91bc868d9524b609a8723cf35956
Benjamin Batres <benbatres1212@gmail.com>
2026-02-07 08:53:45 -0800

    changed grid-cols on tablet to only show 3 per column

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -196,7 +196,7 @@ export default function Home() {
                 </div>
 
                 <div
-                  className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 relative ${filteredByRating.length === 0 && activeTab === "Ratings" ? "justify-center" : "justify-start"}`}
+                  className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 relative ${filteredByRating.length === 0 && activeTab === "Ratings" ? "justify-center" : "justify-start"}`}
                 >
                   {isLoading ? (
                     <SkeletonProductCard productCards={8} />
